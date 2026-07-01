1a17725bd32f8060e81793efd35ee460082dd0b9
guihocosta <guiihocosta@gmail.com>
2026-07-01 16:14:08 -0300

    chore: mudar var de N pra n

--- Problema2.c
+++ Problema2.c
@@ -2,27 +2,27 @@
 
 int main()
 {
-    int N;
+    int n;
     int i = 1;
 
     printf("N: ");
-    scanf("%d", &N);
+    scanf("%d", &n);
 
-    if (N < 1)
+    if (n < 1)
     {
         printf("Invalido! O numero deve ser positivo. \n");
         return 1;
     }
 
-    while (N != 1)
+    while (n != 1)
     {
-        printf("%d \n", N);
-        if (N % 2 == 0) N = N / 2;
-        else N = N * 3 + 1;
+        printf("%d \n", n);
+        if (n % 2 == 0) n = n / 2;
+        else n = n * 3 + 1;
         i++;
     }
 
-    printf("%d \n", N);
+    printf("%d \n", n);
 
     printf("Foram gerados %d termos \n", i);
 
